c6d952a41835b155ab7c3ec6a30fb8115a0484f9
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-05 13:38:52 +0200

    release v21.9.0

--- 1.21.9/Common/src/main/java/fuzs/convenienteffects/client/util/EffectParticleRenderHelper.java
+++ 1.21.9/Common/src/main/java/fuzs/convenienteffects/client/util/EffectParticleRenderHelper.java
@@ -2,16 +2,13 @@ package fuzs.convenienteffects.client.util;
 
 import fuzs.convenienteffects.ConvenientEffects;
 import fuzs.convenienteffects.config.ClientConfig;
-import net.minecraft.CrashReport;
-import net.minecraft.CrashReportCategory;
-import net.minecraft.ReportedException;
+import fuzs.puzzleslib.api.client.util.v1.ClientParticleHelper;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.SingleQuadParticle;
 import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.nbt.NbtOps;
 import net.minecraft.world.entity.Entity;
 
 import java.util.List;
@@ -41,7 +38,8 @@ public class EffectParticleRenderHelper {
             int ambientMultiplier = 5;
             if (entity.getRandom().nextInt(invisibleMultiplier * ambientMultiplier) == 0) {
                 ParticleOptions particleOptions = Util.getRandom(effectParticles, entity.getRandom());
-                Particle particle = addParticle(particleOptions,
+                Particle particle = ClientParticleHelper.addParticle((ClientLevel) entity.level(),
+                        particleOptions,
                         entity.getRandomX(0.5),
                         entity.getRandomY(),
                         entity.getRandomZ(0.5),
@@ -49,51 +47,10 @@ public class EffectParticleRenderHelper {
                         1.0,
                         1.0);
                 // set particle alpha to 15% as vanilla does for ambient effects
-                if (particle != null) particle.setAlpha(0.15F);
+                if (particle instanceof SingleQuadParticle singleQuadParticle) {
+                    singleQuadParticle.alpha = 0.15F;
+                }
             }
-        }
-    }
-
-    /**
-     * Copied from
-     * {@link net.minecraft.client.renderer.LevelRenderer#addParticle(ParticleOptions, double, double, double, double,
-     * double, double)}.
-     * <p>
-     * We must return the created particle.
-     */
-    public static Particle addParticle(ParticleOptions options, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        return addParticle(options, options.getType().getOverrideLimiter(), false, x, y, z, xSpeed, ySpeed, zSpeed);
-    }
-
-    /**
-     * Copied from
-     * {@link net.minecraft.client.renderer.LevelRenderer#addParticle(ParticleOptions, boolean, boolean, double, double,
-     * double, double, double, double)}.
-     * <p>
-     * We must return the created particle.
-     */
-    public static Particle addParticle(ParticleOptions options, boolean force, boolean decreased, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
-        Minecraft minecraft = Minecraft.getInstance();
-        try {
-            return minecraft.levelRenderer.addParticleInternal(options,
-                    force,
-                    decreased,
-                    x,
-                    y,
-                    z,
-                    xSpeed,
-                    ySpeed,
-                    zSpeed);
-        } catch (Throwable var19) {
-            CrashReport crashReport = CrashReport.forThrowable(var19, "Exception while adding particle");
-            CrashReportCategory crashReportCategory = crashReport.addCategory("Particle being added");
-            crashReportCategory.setDetail("ID", BuiltInRegistries.PARTICLE_TYPE.getKey(options.getType()));
-            crashReportCategory.setDetail("Parameters",
-                    () -> ParticleTypes.CODEC.encodeStart(minecraft.level.registryAccess()
-                            .createSerializationContext(NbtOps.INSTANCE), options).toString());
-            crashReportCategory.setDetail("Position",
-                    () -> CrashReportCategory.formatLocation(minecraft.level, x, y, z));
-            throw new ReportedException(crashReport);
         }
     }
 }
