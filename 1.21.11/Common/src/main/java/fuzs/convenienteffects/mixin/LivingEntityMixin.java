4619d79e3bca65684df1ce327c326873d45e8c37
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-12-14 11:39:05 +0100

    prepare 1.21.11 port

---
+++ 1.21.11/Common/src/main/java/fuzs/convenienteffects/mixin/LivingEntityMixin.java
@@ -0,0 +1,26 @@
+package fuzs.convenienteffects.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import fuzs.convenienteffects.ConvenientEffects;
+import fuzs.convenienteffects.config.ServerConfig;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LivingEntity.class)
+abstract class LivingEntityMixin extends Entity {
+
+    public LivingEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @ModifyReturnValue(method = "getEffectiveGravity", at = @At("RETURN"))
+    protected double getEffectiveGravity(double effectiveGravity) {
+        if (!ConvenientEffects.CONFIG.get(ServerConfig.class).slowFallingQuickDescent) return effectiveGravity;
+        return effectiveGravity != this.getGravity() && this.isDescending() ? Math.max(this.getGravity(), 0.01) :
+                effectiveGravity;
+    }
+}
